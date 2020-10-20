f02dfe85db55126e08fb6ff6355b00c568960307
Chaika <alexchaika80@gmail.com>
2020-10-20 16:18:04 -0400

    template to create azure load balancer with multiple rules

---
+++ ARM_ILB_Templ/README.md
@@ -0,0 +1,2 @@
+Template creates Azure Load Balancer and multiple load balancing rules along with health probes as well as backend pools for the specified in the parameter file ports.
+Upon successful deployment virtual machines specified in the deployment script are added to the backend pool of the load balancer.
---
+++ ARM_ILB_Templ/azuredeploy-az.ps1
@@ -0,0 +1,48 @@
+﻿# Variables that need to be set to start working with ARM.
+
+# The subscriptionId
+$subscriptionId = <subscribtion id>
+
+
+# Root path to script, template and parameters.  All have to be in the same folder.
+$rootPathToFiles = Split-Path -Parent $PSCommandPath
+
+# Name of the resource group you are targeting the deployment into
+$resourceGroupName = <resource group name>
+
+# Name of the deployment; User friendly name to reference the deployment.
+$deploymentName = 'AZ-TESTILB'
+
+# Authenticate Against you Azure Tenant
+Login-AzAccount
+
+# List subscriptions that are available to you
+Get-AzSubscription
+
+# Select the subscription you want to work on
+Set-AzContext -SubscriptionId $subscriptionId
+
+# Run the below to test the ARM template
+Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPathToFiles/azuredeploy.json" -TemplateParameterFile "$rootPathToFiles/azuredeploy.parameters.json" #DEVQA
+
+# Deploy the ARM template using the parameter file
+$res = New-AzResourceGroupDeployment -verbose -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "$rootPathToFiles/azuredeploy.json" -TemplateParameterFile "$rootPathToFiles/azuredeploy.parameters.json" #DEVQA
+
+if($res.ProvisioningState -eq "Succeeded"){
+
+    $VMNames = "AZ-TESTVM01","AZ-TESTVM02"
+
+    $LBName = $res.Outputs.lbName.Value
+
+    $slb = Get-AzLoadBalancer | ?{$_.Name -eq $LBName}
+
+    $BkEnd = $slb | Get-AzLoadBalancerBackendAddressPoolConfig
+
+    foreach ($vmName in $VMNames) {
+        $vm = get-AzVm | ?{$_.Name -eq $vmName}
+        $NetIntName = $vm.NetworkProfile.NetworkInterfaces[0].Id | Split-Path -Leaf
+        $NetInt = Get-AzNetworkInterface -Name $NetIntName -ResourceGroupName $vm.ResourceGroupName
+        $NetInt.IpConfigurations[0].LoadBalancerBackendAddressPools=$BkEnd
+        Set-AzNetworkInterface -NetworkInterface $NetInt
+    }
+}
